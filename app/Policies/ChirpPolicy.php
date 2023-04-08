0dc19e791790316773285f342a2dd46d9fa17fef
agyeievans <evanskofiagyei06@gmail.com>
2023-04-08 12:58:53 +0000

    updated policy

--- app/Policies/ChirpPolicy.php
+++ app/Policies/ChirpPolicy.php
@@ -11,56 +11,56 @@ class ChirpPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
+    // public function viewAny(User $user): bool
+    // {
+    //     //
+    // }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Chirp $chirp): bool
-    {
-        //
-    }
+    // public function view(User $user, Chirp $chirp): bool
+    // {
+    //     //
+    // }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
-    {
-        //
-    }
+    // public function create(User $user): bool
+    // {
+    //     //
+    // }
 
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Chirp $chirp): bool
     {
-        //
+        return $chirp->user()->is($user);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Chirp $chirp): bool
-    {
-        //
-    }
+    // public function delete(User $user, Chirp $chirp): bool
+    // {
+    //     //
+    // }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Chirp $chirp): bool
-    {
-        //
-    }
+    // public function restore(User $user, Chirp $chirp): bool
+    // {
+    //     //
+    // }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Chirp $chirp): bool
-    {
-        //
-    }
+    // public function forceDelete(User $user, Chirp $chirp): bool
+//     {
+//         //
+//     }
 }

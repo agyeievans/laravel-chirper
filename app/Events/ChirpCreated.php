7c9ef81d9ce6d7ec295286ab38367e0d9a1d3dd2
agyeievans <evanskofiagyei06@gmail.com>
2023-04-08 13:13:07 +0000

    update event

--- app/Events/ChirpCreated.php
+++ app/Events/ChirpCreated.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\Chirp;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -17,7 +18,7 @@ class ChirpCreated
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
         //
     }
